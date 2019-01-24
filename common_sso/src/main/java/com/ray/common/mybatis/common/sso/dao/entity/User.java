151ccc526ab666a0533fe5e7c6b1e8c4194782dd
Miste <misterchangray@hotmail.com>
2019-01-24 08:31:16 +0800

    提交单点功能

--- common_sso/src/main/java/com/ray/common/mybatis/common/sso/dao/entity/User.java
+++ common_sso/src/main/java/com/ray/common/mybatis/common/sso/dao/entity/User.java
@@ -8,4 +8,40 @@ package com.ray.common.mybatis.common.sso.dao.entity;
  * 描述：
  */
 public class User {
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
